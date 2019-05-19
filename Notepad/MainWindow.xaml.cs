5affcdedea42043605db6e7a8caae175ae9815da
Earl Peter de los Reyes <ken.dlr.otaku@outlook.com>
2019-05-20 02:48:19 +0800

    Add project files.

---
+++ Notepad/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Notepad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow 
+    {
+        public MainWindow()
+        {
+            DataContext = new VM();
+            InitializeComponent();
+        }
+
+        private void AcrylicWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to close?","Close Confirmation",button: MessageBoxButton.YesNo)== MessageBoxResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
---
+++ Notepad/VM.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Notepad
+{
+    public class VM
+    {
+        private int fontSize;
+        public int FontSize
+        {
+            get { return fontSize; }
+            set
+            {
+                if (fontSize != value)
+                {
+                    fontSize = value;
+                    RaisePropertyChanged("FontSize");
+                }
+            }
+        }
+
+        private bool isOnTop;
+        public bool IsOnTop
+        {
+            get { return isOnTop; }
+            set
+            {
+                if (isOnTop != value)
+                {
+                    isOnTop = value;
+                    RaisePropertyChanged("IsOnTop");
+                }
+            }
+        }
+
+        private bool isWrap;
+        public bool IsWrap
+        {
+            get { return isWrap; }
+            set
+            {
+                if (isWrap != value)
+                {
+                    isWrap = value;
+                    RaisePropertyChanged("IsWrap");
+                    Wrapping = value ? TextWrapping.Wrap : TextWrapping.NoWrap;
+                }
+            }
+        }
+
+        private TextWrapping wrapping;
+        public TextWrapping Wrapping
+        {
+            get { return wrapping; }
+            set
+            {
+                if (wrapping != value)
+                {
+                    wrapping = value;
+                    RaisePropertyChanged("Wrapping");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
